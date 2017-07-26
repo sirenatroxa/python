7449a42d65ba4b5bebc57c6e58f5aacbe206c64e
sirenatroxa <sirenatroxa@users.noreply.github.com>
2017-07-26 09:05:38 -0700

    Updated practice files

---
+++ practice/cast.py
@@ -0,0 +1,12 @@
+a = input( "Enter A Number: " )
+b = input( "Now Enter Another Number: " )
+
+sum = a + b 
+print( "\nData Type sum :" , sum , type(sum ) )
+
+
+sum =chr( int( sum ) )
+print( "Data Type sum :" , sum , type( sum ) )
+
+
+
---
+++ practice/logic.py
@@ -0,0 +1,19 @@
+a = True
+b = False
+
+print( "AND Logic:" )
+
+print( "a and a =" , a and a)
+print( "a and a =" , a and a)
+print( "a and a =" , a and a)
+
+print( "\nOR Logic:" )
+
+print( "a or a =" , a or a)
+print( "a or a =" , a or a)
+print( "a or a =" , a or a)
+
+print( "\nNOT Logic:" )
+
+print( "a =" , a , "\tnot a =" , not a )
+print( "b =" , b , "\tnot b =" ,not b )
